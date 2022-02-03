cd2283dfbe882d4bd6af310e32d34e483fc93632
ymktmk <ymktmk.co@gmail.com>
2022-02-04 05:35:30 +0900

    [ADD] timeout

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ RUN mkdir /go/src/work
 WORKDIR /go/src/work
 
 COPY main.go .
+COPY go.mod .
+COPY go.sum .
 
 RUN CGO_ENABLED=0 GOOS=linux go build main.go
 
--- main.go
+++ main.go
@@ -5,20 +5,25 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+	"time"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
-	http.HandleFunc("/api/v1/python", handleExec)
-	http.HandleFunc("/api/v1/php", handleExec)
-	http.HandleFunc("/api/v1/node", handleExec)
-	http.HandleFunc("/api/v1/ruby", handleExec)
-	http.HandleFunc("/api/v1/go", handleExec)
-	http.HandleFunc("/api/v1/dart", handleExec)
-	http.ListenAndServe(":10000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/python", handleExec).Methods("POST")
+	r.HandleFunc("/api/v1/php", handleExec).Methods("POST")
+	r.HandleFunc("/api/v1/node", handleExec).Methods("POST")
+	r.HandleFunc("/api/v1/ruby", handleExec).Methods("POST")
+	r.HandleFunc("/api/v1/go", handleExec).Methods("POST")
+	r.HandleFunc("/api/v1/dart", handleExec).Methods("POST")
+	c := cors.AllowAll().Handler(r)
+	http.ListenAndServe(":10000", c)
 }
 
 type Editor struct {
@@ -28,13 +33,6 @@ type Editor struct {
 
 // *** Python ***
 func handleExec(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Methods","POST")
-	w.Header().Set("Content-Type", "application/json")
-
-	var result string
 
 	// json
 	body := make([]byte, r.ContentLength)
@@ -46,22 +44,49 @@ func handleExec(w http.ResponseWriter, r *http.Request) {
 	str := string(r.URL.String())
 	language := strings.Replace(str, "/api/v1/", "", 1)
 
+	// write
 	file_name := writeFile(editor.Code, language)
-	result = dockerRun(file_name, language)
-	editor.Result = result
 
-	exec.Command(
-		"rm","/go/src/work/" + file_name,
-	).Run()
+	for {
 
-	editor_json, err := json.Marshal(editor)
+        channel := make(chan string)
 
-	if err != nil {
-		fmt.Println(err)
-	}
+        go func() {
+            result := dockerRun(file_name, language)
+            channel <- result
+        }()
+        
+		select {
+			// not time out
+			case result := <-channel:
+				editor.Result = result
+				deleteFile(file_name)
+				response, err := json.Marshal(editor)
+			
+				if err != nil {
+					fmt.Println(err)
+				}
+			
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(response)
+				return
+			// time out
+			case <-time.After(3 * time.Second):
+				dockerKill(language)
+				editor.Result = "Error: TimeOut"
+				deleteFile(file_name)
+				response, err := json.Marshal(editor)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(editor_json)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(response)
+                return
+		}
+    }
+
 }
 
 func writeFile(code string, language string) string {
@@ -101,6 +126,7 @@ func dockerRun(file_name string, language string) string {
 
 	cmd := exec.Command(
 		"docker","run","-i","--rm",
+		"--name",language,
 		"--volumes-from","code",
 		"-w","/go/src/work",
 		language,
@@ -124,3 +150,14 @@ func dockerRun(file_name string, language string) string {
 	return string(out)
 }
 
+func dockerKill(language string) {
+	exec.Command(
+        "docker","rm","-f",language,
+    ).Run()
+}
+
+func deleteFile(file_name string) {
+	exec.Command(
+		"rm","/go/src/work/" + file_name,
+	).Run()
+}
