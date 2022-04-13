ed918732748ace096d0029789159666042be800f
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-04-14 00:47:51 +0900

    [fix] provider.tf

--- MANUAL.md
+++ MANUAL.md
@@ -1,6 +1,18 @@
 ## 課題
 ## Terraform apply と ECS task更新の順序
-## TerraformファイルをS3に保存するのか？
+
+## 環境構築手順
+## S3とDynamoDBでterraform.tfstateをS3を管理するように設定
+
+```
+terraform apply -target={aws_s3_bucket.terraform_state,aws_dynamodb_table.terraform_state_lock}
+```
+
+## ECRを作成する
+
+```
+terraform apply -target={aws_ecr_repository.ecr_repository,aws_ecr_lifecycle_policy.ecr_lifecycle_policy}
+```
 
 ## デプロイ後にやること
 ## 言語別のコンテナイメージをPull
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,7 @@ terraform {
         }
     }
     backend "s3" {
-        bucket = "terraform-state"
+        bucket = "terraform-state-ymktmk"
         region = "ap-northeast-1"
         profile = "default"
         key = "terraform.tfstate"
@@ -22,7 +22,7 @@ terraform {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-state"
+    bucket = "terraform-state-ymktmk"
     lifecycle {
         prevent_destroy = true
     }
