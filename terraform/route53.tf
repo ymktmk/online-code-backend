61c53a598df90df91ec3035613209b8cde1e528a
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-05-24 18:36:10 +0900

    [fix] route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,14 +3,11 @@ resource "aws_route53_zone" "zone" {
       force_destroy = false
 }
 
-# CNAMEレコード
-# Webサイトのドメイン
-
 # APIのドメイン
-# resource "aws_route53_record" "api" {
-#       zone_id = aws_route53_zone.zone.zone_id
-#       name    = "api.code-run.ga"
-#       type    = "A"
-#       ttl     = "300"
-#       records = [aws_eip.eip.public_ip]
-# }
+resource "aws_route53_record" "api" {
+      zone_id = aws_route53_zone.zone.zone_id
+      name    = "api.code-run.ga"
+      type    = "A"
+      ttl     = "300"
+      records = [aws_eip.eip.public_ip]
+}
